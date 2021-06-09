73a4b6f2466731274a685d104cdb435987eb44bb
mpierce1988 <80133921+mpierce1988@users.noreply.github.com>
2021-06-09 15:05:07 -0300

    Basic Player Movement Script, moved animal prefabs to Prefabs folder

--- programming-theory/Assets/PlayerMovement.cs
+++ programming-theory/Assets/PlayerMovement.cs
@@ -1,18 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
+[RequireComponent(typeof(NavMeshAgent))]
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField]
+    private float _movementSpeed;
+
+    private NavMeshAgent _navMeshAgent;
+
+    private void Awake()
+    {
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+        //_navMeshAgent.updatePosition = false;
+        _movementSpeed = _navMeshAgent.speed;
+        _navMeshAgent.autoBraking = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 inputDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        Debug.Log("InputDirection: " + inputDirection);
+
+        Vector2 desiredMovement = inputDirection * (_movementSpeed * Time.deltaTime);
+        Vector3 desiredMovementVector3 = new Vector3(desiredMovement.x, 0f, desiredMovement.y);
+
+        //_navMeshAgent.Move(desiredMovementVector3);
+        Vector3 desiredPosition = transform.position + desiredMovementVector3;
+        _navMeshAgent.SetDestination(desiredPosition);
     }
 }
