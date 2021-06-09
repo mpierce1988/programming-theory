156b28b15f526dd127d1f72bd3877b4d7ae588b1
mpierce1988 <80133921+mpierce1988@users.noreply.github.com>
2021-06-09 13:39:27 -0300

    Add OnMenuLoaded and OnGameplayLoaded events to SceneLoader class

--- programming-theory/Assets/SceneLoader.cs
+++ programming-theory/Assets/SceneLoader.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System;
 
 public class SceneLoader : MonoBehaviour
 {
@@ -11,13 +12,18 @@ public class SceneLoader : MonoBehaviour
     [SerializeField]
     private const string _gameplayName = "gameplay";
 
+    public event Action OnMenuLoaded = delegate { };
+    public event Action OnGameplayLoaded = delegate { };
+
     public void LoadMenu()
     {
         SceneManager.LoadScene(_menuName);
+        OnMenuLoaded.Invoke();
     }
 
     public void LoadGameplay()
     {
         SceneManager.LoadScene(_gameplayName);
+        OnGameplayLoaded.Invoke();
     }
 }
